cf7ac9325ca41665562a5dbf17995ac2076e6e67
Alex5 <Alex5@TOLEDO521>
2023-10-28 15:14:41 -0600

    Agregar archivos de proyecto.

---
+++ TABLAS DE MULTIPLICAR CON WHILE/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace TABLA_DE_MULTIPLICAR_CON_WHILE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NUM, I, RESUL;
+            string linea;
+            Console.WriteLine("DIGITE NUMERO: "); linea = Console.ReadLine();
+            NUM = int.Parse(linea);
+            I = 1;
+            while((I <= 12))
+            {
+                RESUL = NUM * I;
+                Console.WriteLine("{0} * {1} * = {2} ", NUM, I, RESUL);
+                I = I + 1;
+            }
+            Console.WriteLine("PULSE UNA TECLA: "); Console.ReadLine();
+        }
+    }
+}
